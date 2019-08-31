24b3073c4ce3ef59d74fe4dd57df519ce028b76a
Elliot Jordan <elliot@elliotjordan.com>
2019-08-31 16:37:53 -0700

    Resolve indentation and formatting issues

--- SweetHome3D/ETeksURLProvider.py
+++ SweetHome3D/ETeksURLProvider.py
@@ -16,7 +16,6 @@
 """See docstring for ETeksURLProvider"""
 
 from __future__ import absolute_import
-import os
 import urllib2
 import xml.etree.ElementTree as ET
 
@@ -35,40 +34,40 @@ class ETeksURLProvider(Processor):
     description = __doc__
     input_variables = {
         "product_name": {
-	    "required": True,
-	    "description": "Which product to fetch: Installer, etc...",
-	},
+            "required": True,
+            "description": "Which product to fetch: Installer, etc...",
+        },
     }
     output_variables = {
         "url": {
-	    "description": "URL to latest version of the product.",
-	},
+            "description": "URL to latest version of the product.",
+        },
     }
 
     def main(self):
         """Provide a download URL for Sweet Home 3D"""
-	valid_prods = prods.keys()
-	product_name = self.env["product_name"]
-	if product_name not in valid_prods:
-	    raise ProcessorError("product_name %s is invalid" % (product_name) )
+        valid_prods = prods.keys()
+        product_name = self.env["product_name"]
+        if product_name not in valid_prods:
+            raise ProcessorError("product_name %s is invalid" % (product_name))
 
-	# Get the xml file of updates
-	try:
-	    fref = urllib2.urlopen(check_url)
-	    xmldata = fref.read()
-	    fref.close()
-	except BaseException as err:
-	    raise ProcessorError("Can't download %s: %s" % (check_url, err))
+        # Get the xml file of updates
+        try:
+            fref = urllib2.urlopen(check_url)
+            xmldata = fref.read()
+            fref.close()
+        except BaseException as err:
+            raise ProcessorError("Can't download %s: %s" % (check_url, err))
 
-	# Create download link
-	tree = ET.fromstring(xmldata)
-	for installer in tree.findall("./update/[@id='SweetHome3D#%s']" % product_name):
-	    if (installer.attrib['operatingSystem'] == 'Mac OS X'):
-		version=installer.attrib['version']
+        # Create download link
+        tree = ET.fromstring(xmldata)
+        for installer in tree.findall("./update/[@id='SweetHome3D#%s']" % product_name):
+            if installer.attrib['operatingSystem'] == 'Mac OS X':
+                version = installer.attrib['version']
 
-	download_url="%s/SweetHome3D-%s/SweetHome3D-%s-macosx.dmg" % (base_prod_url, version, version)
-	self.env["url"] = download_url
-	self.output("Found URL as %s" % self.env["url"])
+        download_url = "%s/SweetHome3D-%s/SweetHome3D-%s-macosx.dmg" % (base_prod_url, version, version)
+        self.env["url"] = download_url
+        self.output("Found URL as %s" % self.env["url"])
 
 if __name__ == "__main__":
     PROCESSOR = ETeksURLProvider()
