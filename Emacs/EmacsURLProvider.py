375380535d46450a7cebf2cf22b9cd146d1cf274
Elliot Jordan <elliot@elliotjordan.com>
2020-01-03 23:38:26 -0800

    Switch processor from subprocess curl to URLGetter

--- Emacs/EmacsURLProvider.py
+++ Emacs/EmacsURLProvider.py
@@ -17,47 +17,42 @@
 
 from __future__ import absolute_import
 
-import subprocess
 import xml.etree.ElementTree as ET
 
-from autopkglib import Processor, ProcessorError
+from autopkglib import Processor, ProcessorError, URLGetter
 
 __all__ = ["EmacsURLProvider"]
 
 check_url = "https://emacsformacosx.com/atom/release"
 
 
-class EmacsURLProvider(Processor):
+class EmacsURLProvider(URLGetter):
     """This processor obtains a download URL for the latest version of GNU Emacs for OSX"""
+
     description = __doc__
-    input_variables = {
-    }
+    input_variables = {}
     output_variables = {
-        "url": {
-            "description": "URL to latest version of the product.",
-        },
+        "url": {"description": "URL to latest version of the product.",},
     }
 
     def main(self):
         """Provide a download URL for GNU Emacs for OSX"""
         # Get the xml file of updates
         try:
-            xmldata = subprocess.check_output(('/usr/bin/curl',
-                                               '--silent',
-                                               '--location',
-                                               check_url))
+            xmldata = self.download(check_url)
         except BaseException as err:
             raise ProcessorError("Can't download %s: %s" % (check_url, err))
 
         # Grab the first download link of the right type
         root = ET.fromstring(xmldata)
-        for link in root.iter('{http://www.w3.org/2005/Atom}link'):
-            if link.attrib['type'] == 'binary/octet-stream':
-                url = link.attrib['href']
+        for link in root.iter("{http://www.w3.org/2005/Atom}link"):
+            if link.attrib["type"] == "binary/octet-stream":
+                url = link.attrib["href"]
                 break
 
         self.env["url"] = url
         self.output("Found URL as %s" % self.env["url"])
+
 
 if __name__ == "__main__":
     PROCESSOR = EmacsURLProvider()
