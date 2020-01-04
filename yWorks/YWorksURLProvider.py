e836c13cc57675a349fa1bf684753a97eb49b20d
Elliot Jordan <elliot@elliotjordan.com>
2020-01-03 23:35:49 -0800

    Update URLProvider processors to use URLGetter

--- yWorks/YWorksURLProvider.py
+++ yWorks/YWorksURLProvider.py
@@ -17,29 +17,23 @@
 
 from __future__ import absolute_import
 
-from autopkglib import Processor, ProcessorError
-
-try:
-    from urllib.request import urlopen  # For Python 3
-except ImportError:
-    from urllib2 import urlopen  # For Python 2
+from autopkglib import Processor, ProcessorError, URLGetter
 
 __all__ = ["YWorksURLProvider"]
 
-class YWorksURLProvider(Processor):
+
+class YWorksURLProvider(URLGetter):
     """This processor obtains a download URL for the latest version of yWorks"""
+
     description = __doc__
     input_variables = {
         "product_name": {
             "required": True,
-            "description":
-                "Product to fetch URL for. Right now, only 'yEd'.",
+            "description": "Product to fetch URL for. Right now, only 'yEd'.",
         },
     }
     output_variables = {
-        "url": {
-            "description": "URL to latest version of the given product.",
-        },
+        "url": {"description": "URL to latest version of the given product.",},
     }
 
     def main(self):
@@ -47,22 +41,31 @@ class YWorksURLProvider(Processor):
         product_name = self.env["product_name"]
         # http://www.yworks.com/products/yed/demo/yEd-CurrentVersion.txt
         base_url = "http://www.yworks.com/products"
-        check_url = "%s/%s/demo/%s-CurrentVersion.txt" % (base_url, product_name.lower(), product_name)
+        check_url = "%s/%s/demo/%s-CurrentVersion.txt" % (
+            base_url,
+            product_name.lower(),
+            product_name,
+        )
+        print(check_url)
 
         # Get the text file
         try:
-            fref = urlopen(check_url)
-            txt = fref.read()
-            fref.close()
+            txt = self.download(check_url)
         except BaseException as err:
             raise ProcessorError("Can't download %s: %s" % (check_url, err))
 
         # Create download link
         latest = txt.rstrip()
         base_prod_url = "http://www.yworks.com/products"
-        download_url = "%s/%s/demo/%s-%s_with-JRE10.dmg" % (base_prod_url, product_name.lower(), product_name, latest)
+        download_url = "%s/%s/demo/%s-%s_with-JRE10.dmg" % (
+            base_prod_url,
+            product_name.lower(),
+            product_name,
+            latest,
+        )
         self.env["url"] = download_url
         self.output("Found URL as %s" % self.env["url"])
+
 
 if __name__ == "__main__":
     PROCESSOR = YWorksURLProvider()
